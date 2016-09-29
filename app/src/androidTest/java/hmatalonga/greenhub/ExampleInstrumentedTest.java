75d9db3d9122ad9cb3d5d434a47bdf1399d973d9
Hugo Matalonga <hmatalonga@gmail.com>
2016-09-29 18:08:54 +0100

    Update gradle build and travis profile

--- app/src/androidTest/java/hmatalonga/greenhub/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/hmatalonga/greenhub/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.hugo.myapplication", appContext.getPackageName());
+        assertEquals("hmatalonga.greenhub", appContext.getPackageName());
     }
 }
