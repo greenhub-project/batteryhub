a24aaef765eec98cfb4f3ac81851ceebcde21d0b
Hugo Matalonga <hmatalonga@gmail.com>
2016-10-14 11:37:43 +0100

    Rebase branch with webapp

---
+++ app/scripts/main.js
@@ -0,0 +1,50 @@
+google.charts.load('current', {'packages':['gantt']});
+google.charts.setOnLoadCallback(drawChart);
+
+function daysToMilliseconds(days) {
+      return days * 24 * 60 * 60 * 1000;
+}
+
+function drawChart() {
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Task ID');
+  data.addColumn('string', 'Task Name');
+  data.addColumn('string', 'Resource');
+  data.addColumn('date', 'Start Date');
+  data.addColumn('date', 'End Date');
+  data.addColumn('number', 'Duration');
+  data.addColumn('number', 'Percent Complete');
+  data.addColumn('string', 'Dependencies');
+
+  data.addRows([
+    ['repo-docs', 'Repository documentation', 'docs',
+      new Date(2016, 9, 1), null, daysToMilliseconds(21), 30, null],
+    ['workflow', 'Workflow', 'task',
+      new Date(2016, 9, 1), null, daysToMilliseconds(14), 75, null],
+    ['travisCI', 'Continuous integration', 'tool',
+      new Date(2016, 9, 1), null, daysToMilliseconds(7), 100, null],
+    ['docker', 'Docker containerization', 'tool',
+      null, null, daysToMilliseconds(7), 50, 'travisCI'],
+    ['tests', 'Tests Implementation', 'testing',
+      new Date(2016, 9, 15), null, daysToMilliseconds(30), 0, null],
+    ['code-guidelines', 'Change project guidelines', 'docs',
+      new Date(2016, 9, 15), null, daysToMilliseconds(14), 0, null],
+    ['retrofit', 'Migrate HTTP library', 'development',
+      null, null, daysToMilliseconds(7), 0, 'code-guidelines'],
+    ['alpha', 'Alpha stage', 'alpha',
+      new Date(2016, 10, 5), null, daysToMilliseconds(14), 0, null],
+    ['beta', 'Beta stage', 'beta',
+      new Date(2016, 10, 19), null, daysToMilliseconds(30), 0, 'alpha'],
+    ['release-stable', 'Stable release', 'release',
+      new Date(2016, 11, 19), null, daysToMilliseconds(7), 0, 'beta']
+  ]);
+
+  var options = {
+    height: 500
+  };
+
+  var chart = new google.visualization.Gantt(document.getElementById('chart_roadmap'));
+
+  chart.draw(data, options);
+}
--- app/src/main/java/hmatalonga/greenhub/utils/Tracker.java
+++
@@ -1,7 +0,0 @@
-package hmatalonga.greenhub.utils;
-
-/**
- * Created by hugo on 06-03-2016.
- */
-public class Tracker {
-}
