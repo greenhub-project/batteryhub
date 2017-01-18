747cf3da9988176fe8ed5defd18c75dc6d6bb7d1
Hugo Matalonga <hmatalonga@gmail.com>
2017-01-18 22:09:36 +0000

    Add features to landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ google.charts.load('current', {'packages':['gantt']});
 google.charts.setOnLoadCallback(drawChart);
 
 function daysToMilliseconds(days) {
-      return days * 24 * 60 * 60 * 1000;
+  return days * 24 * 60 * 60 * 1000;
 }
 
 function drawChart() {
@@ -23,7 +23,7 @@ function drawChart() {
     ['tests', 'Tests Implementation', 'testing',
       new Date(2017, 0, 1), null, daysToMilliseconds(30), 55, null],
     ['alpha', 'Alpha stage', 'alpha',
-      new Date(2017, 0, 1), null, daysToMilliseconds(19), 80, null],
+      new Date(2017, 0, 7), null, daysToMilliseconds(13), 75, null],
     ['beta', 'Beta stage', 'beta',
       new Date(2017, 0, 20), null, daysToMilliseconds(21), 0, 'alpha'],
     ['release-stable', 'Stable release', 'release',
