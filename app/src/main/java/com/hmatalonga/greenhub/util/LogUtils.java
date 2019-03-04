66076257616f915c2f13928c59ab8d69942a6750
Marco Couto <marcocouto90@gmail.com>
2019-03-04 16:30:08 +0000

    fix log issues

--- app/src/main/java/com/hmatalonga/greenhub/util/LogUtils.java
+++ app/src/main/java/com/hmatalonga/greenhub/util/LogUtils.java
@@ -25,7 +25,7 @@ import android.util.Log;
 public class LogUtils {
     private static final String LOG_PREFIX = "batteryhub_";
     private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
-    private static final int MAX_LOG_TAG_LENGTH = 25;
+    private static final int MAX_LOG_TAG_LENGTH = 23;
 
     public static boolean LOGGING_ENABLED = false;
 
@@ -45,49 +45,49 @@ public class LogUtils {
     }
 
     public static void logD(final String tag, String message) {
-        if (LOGGING_ENABLED && Log.isLoggable(tag, Log.DEBUG)) {
+        if (LOGGING_ENABLED && message != null && Log.isLoggable(tag, Log.DEBUG)) {
             Log.d(tag, message);
         }
     }
 
     public static void logD(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED && Log.isLoggable(tag, Log.DEBUG)) {
+        if (LOGGING_ENABLED && message != null && Log.isLoggable(tag, Log.DEBUG)) {
             Log.d(tag, message, cause);
         }
     }
 
     public static void logI(final String tag, String message) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.i(tag, message);
         }
     }
 
     public static void logI(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.i(tag, message, cause);
         }
     }
 
     public static void logW(final String tag, String message) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.w(tag, message);
         }
     }
 
     public static void logW(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.w(tag, message, cause);
         }
     }
 
     public static void logE(final String tag, String message) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.e(tag, message);
         }
     }
 
     public static void logE(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
+        if (LOGGING_ENABLED && message != null) {
             Log.e(tag, message, cause);
         }
     }
